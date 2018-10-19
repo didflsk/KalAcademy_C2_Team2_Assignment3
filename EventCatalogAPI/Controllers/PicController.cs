dc827d7f715602b32db6c80d4b36dc953acb3c14
yukbu406 <42500041+yukbu406@users.noreply.github.com>
2018-10-19 00:06:57 -0700

    Added pictures and uncommented out codes for PicController

--- EventCatalogAPI/Controllers/PicController.cs
+++ EventCatalogAPI/Controllers/PicController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,20 +11,23 @@ namespace EventCatalogAPI.Controllers
 {
     [Produces("application/json")]
     [Route("api/Pic")]
+
     public class PicController : Controller
     {
-        ///private readonly IHostingEnviroment _ env;
-        ///public PicController(IHostingEnvironment env)
-        /// {
-        ///   _env = env;
-        /// }
-        /// [Route("{id}"]
-        /// public IActionResult GetImage(int id)
-        /// {
-        /// var webRoot = env.WebRootPath;
-        /// var path = Path.Combine(webRoot+ "/pics/", "event-" + id + ".png or .jpg");
-        /// var buffer = System.IO.File.ReadAllBytes(path);
-        /// return File(buffer, "image/png or image/jpg");
 
+        private readonly IHostingEnvironment _env;
+        public PicController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot =_env.WebRootPath;
+            var path = Path.Combine(webRoot + "/pics/", "events-" + id + ".jpg");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/jpg");
+
+        }
     }
 }
---
+++ EventCatalogAPI/Migrations/20181016205155_InitialMigration.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EventCatalogAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "catalog_date_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "catalog_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "catalog_location_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "catalog_type_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "CatalogDate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogDate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogLocation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Location = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogLocation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    CatalogDateId = table.Column<int>(nullable: false),
+                    CatalogLocationId = table.Column<int>(nullable: false),
+                    CatalogTypeId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Fee = table.Column<decimal>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    PictureUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogDate_CatalogDateId",
+                        column: x => x.CatalogDateId,
+                        principalTable: "CatalogDate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogLocation_CatalogLocationId",
+                        column: x => x.CatalogLocationId,
+                        principalTable: "CatalogLocation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogType_CatalogTypeId",
+                        column: x => x.CatalogTypeId,
+                        principalTable: "CatalogType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogDateId",
+                table: "Catalog",
+                column: "CatalogDateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogLocationId",
+                table: "Catalog",
+                column: "CatalogLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogTypeId",
+                table: "Catalog",
+                column: "CatalogTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Catalog");
+
+            migrationBuilder.DropTable(
+                name: "CatalogDate");
+
+            migrationBuilder.DropTable(
+                name: "CatalogLocation");
+
+            migrationBuilder.DropTable(
+                name: "CatalogType");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_date_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_location_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "catalog_type_hilo");
+        }
+    }
+}
